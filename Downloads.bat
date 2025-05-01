32bdb6ded091517de0b763a4547aab68d40d7fd8
wowsha22 <wowshagithub@gmail.com>
2025-05-01 17:06:24 +0200

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -19,9 +19,9 @@ set password=@#password
 powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 start "" "rustdesk.exe"
 python login.py
-set "ROBLOX_STUDIO_URL=https://setup.rbxcdn.com/RobloxStudioLauncherBeta.exe"
-set "ROBLOX_STUDIO_EXE=RobloxStudioInstaller.exe"
-curl -o "%ROBLOX_STUDIO_EXE%" "%ROBLOX_STUDIO_URL%"
-if exist "%ROBLOX_STUDIO_EXE%" start "" "%ROBLOX_STUDIO_EXE%"
+set "BLUESTACKS_URL=https://cdn3.bluestacks.com/downloads/windows/bgp/Macaron/4.280.1.1002/BlueStacks-Installer_4.280.1.1002_native.exe"
+set "BLUESTACKS_EXE=BlueStacks4_Nougat32.exe"
+curl -o "%BLUESTACKS_EXE%" "%BLUESTACKS_URL%"
+if exist "%BLUESTACKS_EXE%" start "" "%BLUESTACKS_EXE%"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 tzutil /s "Sri Lanka Standard Time"

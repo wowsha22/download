1611a2d5277108bc809c0a1d14a388f4cf32b505
wowsha22 <wowshagithub@gmail.com>
2025-05-01 12:51:14 +0200

    Update login.py

--- login.py
+++ login.py
@@ -44,7 +44,7 @@ def save_echo_to_batch(file_path, echo_text):
 
 def run_rustdesk_command():
     clipboard_text = pyperclip.paste()
-    password_echo = 'Password : Disalardp1'  
+    password_echo = 'Password : wowya'  
     save_echo_to_batch('show.bat', f'RustDesk ID: {clipboard_text}')
     save_echo_to_batch('show.bat', password_echo)
 

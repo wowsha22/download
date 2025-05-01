9427e9d68bd72ee0907a1fcf546664045fc4d116
wowsha22 <wowshagithub@gmail.com>
2025-05-01 13:29:10 +0200

    Update timelimit.py

--- timelimit.py
+++ timelimit.py
@@ -2,7 +2,7 @@ import psutil
 import subprocess
 import time
 
-threshold_percentage = 30
+threshold_percentage = 99
 excluded_processes = ['devenv.exe', 'HopToDesk.exe', 'msedge.exe']  # List of process names to exclude
 
 while True:

